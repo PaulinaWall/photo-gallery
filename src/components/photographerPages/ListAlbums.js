2842708460190f20f8ffe4bd1bdbb301c3810a39
Paulina Wall <paulina_wall@outlook.com>
2021-01-08 13:09:36 +0100

    rewrote create and listAlbum to structure data better

--- src/components/photographerPages/ListAlbums.js
+++ src/components/photographerPages/ListAlbums.js
@@ -1,39 +1,33 @@
 import React, {useState} from 'react';
-import { Container, Card, Row, Col, Button, Modal, Form } from 'react-bootstrap';
+import { Container, Card, Row, Col, Button, Modal, Form, Alert } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
 import { BarLoader } from 'react-spinners';
 
 import polaroid1276996_1920 from '../../assets/images/polaroid-1276996_1920.jpg'
-import useGetPhotographerGallery from '../../hooks/useGetPhotographerGallery';
+import useGetAlbums from '../../hooks/useGetAlbums'
 import { useAuth } from '../../contexts/AuthContext';
 import { db } from '../../firebase';
 
 const ListAlbums = () => {
 	const [index, setIndex] = useState();
-	const { gallery, loading } = useGetPhotographerGallery();
 	const { currentUser } = useAuth();
 	const [show, setShow] = useState(false);
 	const [albumTitle, setAlbumTitle] = useState('');
+	const { albums, loading } = useGetAlbums();
+	const [error, setError] = useState(false);
 
 	const handleClose = () => setShow(false);
 	const handleShow = (index) => {setShow(true); setIndex(index);}
 
 	const handleSave = async () => {
 		try {
-			await db.collection('galleries').doc(gallery.id).get()
-			.then((snapshot) => {
-				const albums = snapshot.data().albums;
-				const album = snapshot.data().albums[index];
-				album.albumTitle = albumTitle;
-				albums[index] = album;
-				db.collection('galleries').doc(gallery.id).set({
-					owner: currentUser.uid,
-					albums
-				})
+			await db.collection('albums').doc(albums[index].id).set({
+				albumTitle,
+				owner: currentUser.uid,
 			})
 		} catch (e) {
-			console.log(e.message);
+			setError(e.message);
 		}
 		setAlbumTitle('');
 		setShow(false);
@@ -41,6 +35,9 @@ const ListAlbums = () => {
 
 	return (
 		<Container className="mt-3 mb-4">
+			{
+				error && <Alert variant="danger">{error}</Alert>
+			}
 			<div className="d-flex justify-content-center">
 				<Image className="jumbotron-image" src={polaroid1276996_1920} fluid />
 			</div>
@@ -50,25 +47,25 @@ const ListAlbums = () => {
 					: (
 						<Row>
 							{
-								gallery.albums.map((album, index) => (
-									<Col className="mt-3" sm={12} md={4} lg={4} key={index}>
-										<Card className="album-card">
-											<Card.Body className="pb-0">
-												<Card.Title>
-													<div className="album-links">
-														<Link className="mb-3" to={`/${currentUser.email}/${index}`}>
-															{album.albumTitle}
-														</Link>
-													</div>
-												</Card.Title>
-											</Card.Body>
-											<Button onClick={() => handleShow(index)}>Change Album Title</Button>
-										</Card>
-									</Col>
+								albums.map((album, index) => (
+									(album.owner === currentUser.uid) && (
+										<Col className="mt-3" sm={12} md={4} lg={4} key={index}>
+											<Card className="album-card">
+												<Card.Body className="pb-0">
+													<Card.Title>
+														<div className="album-links">
+															<Link className="mb-3" to={`/${currentUser.email}/${album.id}`}>
+																{album.albumTitle}
+															</Link>
+														</div>
+													</Card.Title>
+												</Card.Body>
+												<Button onClick={() => handleShow(index)}>Change Album Title</Button>
+											</Card>
+										</Col>
+									)
 								))
-
 							}
-
 						</Row>
 					)
 			}
---
+++ src/hooks/useGetAlbums.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
+
+const useGetAlbums = () => {
+	const [albums, setAlbums] = useState([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const unsubscribe = db.collection('albums').onSnapshot(snapshot => {
+			setLoading(true)
+			const snapshotAlbums = []
+
+			snapshot.forEach(doc => {
+				snapshotAlbums.push({
+					id: doc.id,
+					...doc.data(),
+				})
+			})
+
+			setAlbums(snapshotAlbums)
+			setLoading(false)
+		})
+
+		return unsubscribe
+	}, [])
+
+	return { albums, loading }
+}
+
+export default useGetAlbums
