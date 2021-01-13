07674696d969ac70cb7a19dbaaec5e7219b4e919
Paulina Wall <paulina_wall@outlook.com>
2021-01-13 10:24:04 +0100

    refactoring and fix customerUrl bug to show when and not disapere when rerender

---
+++ src/components/photographerPages/ImageCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const ImageCard = ({ album, image, handleOnDelete, handleCheckOnClick }) => {
+	return (
+		<Card className="mb-3">				
+			<Card.Header>
+			{
+				(!album.fromCustomer) &&
+					<div className="d-flex justify-content-between">
+						<FontAwesomeIcon
+							className={image.checked ? 'checkedIcon' : 'unCheckedIcon'} 
+							icon={faCheck} 
+							onClick={handleCheckOnClick}
+						/>
+						<FontAwesomeIcon
+							className="delete-icon"
+							icon={faTimes} 
+							onClick={handleOnDelete}
+						/>
+					</div>
+			}
+				
+			</Card.Header>
+			<a href={image.url} title="View image in lightbox" data-attribute="SRL">
+				<Card.Img variant="top" src={image.url} title={image.name} />
+			</a>
+			<Card.Body>
+				<Card.Text className="text-muted small">
+					{image.name} ({Math.round(image.size/1024)} kb)
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	)
+}
+
+export default ImageCard;
--- src/components/photographerPages/Images.js
+++ src/components/photographerPages/Images.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Card, Button, Container, Modal, Form, Alert } from 'react-bootstrap';
+import { Row, Col, Button, Container, Modal, Form, Alert } from 'react-bootstrap';
 import { SRLWrapper } from 'simple-react-lightbox';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { db } from '../../firebase';
 import { useAuth } from '../../contexts/AuthContext';
 import useDeleteImage from '../../hooks/useDeleteImage';
+import CustomerUrlPage from './CustomerUrlPage';
+import ImageCard from './ImageCard';
 
 const Images = ({ images, album }) => {
 	const navigate = useNavigate()
 	const { albumId } = useParams();
 	const { currentUser } = useAuth();
-	const [customerUrl, setCustomerUrl] = useState('');
 	const [albumTitle, setAlbumTitle] = useState('');
 	const [show, setShow] = useState(false);
-	const [showModal, setShowModal] = useState(false);
 	const [error, setError] = useState();
 	const [imageToDelete, setImageToDelete] = useState(null);
 	useDeleteImage(imageToDelete, albumId);
@@ -34,9 +32,9 @@ const Images = ({ images, album }) => {
 		return checkedImages;
 	};
 
-	const handleCheckOnClick = (index) => {
+	const handleCheckOnClick = async (index) => {
 		try{
-			db.collection('albums').doc(albumId).get().then((doc) => {
+			await db.collection('albums').doc(albumId).get().then((doc) => {
 				const images = doc.data().images;
 				const image = doc.data().images[index];
 				image.checked = !image.checked;
@@ -51,10 +49,10 @@ const Images = ({ images, album }) => {
 		}
 	};
 
-	const handleSaveNewAlbum = () => {
+	const handleSaveNewAlbum = async () => {
 		const checkedImages = filterCheckedImages();
 		try{
-			db.collection('albums').add({
+			await db.collection('albums').add({
 				albumTitle,
 				fromCustomer: false,
 				owner: currentUser.uid,
@@ -69,20 +67,16 @@ const Images = ({ images, album }) => {
 		}
 	};
 	
-	const handleCreateCustomerOnClick = () => {
+	const handleCreateCustomerOnClick = async () => {
 		const checkedImages = filterCheckedImages();
 		try{
-			db.collection('albums').doc(albumId).set({
+			await db.collection('albums').doc(albumId).set({
+				customerUrl: `${window.location.origin}/review/${albumId}`,
 				albumTitle,
 				fromCustomer: false,
 				owner: currentUser.uid,
 				images: checkedImages,
 			})
-			.then(() => {
-				setCustomerUrl(`${window.location.origin}/review/${albumId}`);
-				setShowModal(true);
-			})
-			
 		} catch (e) {
 			setError(e.message);
 		}
@@ -100,48 +94,32 @@ const Images = ({ images, album }) => {
 						error && <Alert variant="danger">{error}</Alert>
 					}
 					{
-					images && images.map((image, index) => {
-						
-						return	<Col sm={6} md={4} lg={3} key={index}>
-								<Card className="mb-3">
-									
-									<Card.Header>
-									{
-										!album.fromCustomer &&
-											<div className="d-flex justify-content-between">
-												<FontAwesomeIcon
-													className={image.checked ? 'checkedIcon' : 'unCheckedIcon'} 
-													icon={faCheck} 
-													onClick={() => handleCheckOnClick(index)}
-												/>
-												<FontAwesomeIcon
-													className="delete-icon"
-													icon={faTimes} 
-													onClick={() => handleOnDelete(index)}
-												/>
-											</div>
-									}
-										
-									</Card.Header>
-									<a href={image.url} title="View image in lightbox" data-attribute="SRL">
-										<Card.Img variant="top" src={image.url} title={image.name} />
-									</a>
-									<Card.Body>
-										<Card.Text className="text-muted small">
-											{image.name} ({Math.round(image.size/1024)} kb)
-										</Card.Text>
-									</Card.Body>
-								</Card>
+						images && images.map((image, index) => (
+							<Col sm={6} md={4} lg={3} key={index}>
+								<ImageCard
+									album={album}
+									image={image}
+									handleOnDelete={() => handleOnDelete(index)}
+									handleCheckOnClick={() => handleCheckOnClick(index)}
+								/>
 							</Col>
-						})}
+						))
+					}
 				</Row>
 			</SRLWrapper>
+
+			
+
 			<div className="mb-3 d-flex flex-column">
 				{
 					!album.fromCustomer
 						? 
 							<>
-								<Button className="ml-auto mb-2" onClick={handleCreateCustomerOnClick}>Create gallery for customer</Button>
+								{
+									album.customerUrl
+										? <CustomerUrlPage customerUrl={album.customerUrl} />
+										: <Button className="ml-auto mb-2" onClick={handleCreateCustomerOnClick}>Create gallery for customer</Button>
+								}
 
 								<Button onClick={() => setShow(true)} className="ml-auto">Create new album with marked images</Button>
 							</>
@@ -173,21 +151,6 @@ const Images = ({ images, album }) => {
 					Create updated Album
 				</Button>
 				</Modal.Footer>
-			</Modal>
-
-			<Modal
-				animation={false}
-				size="sm"
-				show={showModal}
-				aria-labelledby="example-modal-sizes-title-sm"
-				onHide={() => setShowModal(false)}
-			>
-				<Modal.Header closeButton>
-				<Modal.Title id="example-modal-sizes-title-sm">
-					Wow good choice, here is the URL!
-				</Modal.Title>
-				</Modal.Header>
-				<Modal.Body><Link to={customerUrl}>{customerUrl}</Link></Modal.Body>
 			</Modal>
 		</Container>
 	)
